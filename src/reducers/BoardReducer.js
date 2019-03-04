ab833191b631dc2a083259334cd41798f1097878
Slobodan Djordjevic <slobodan.djordjevic@carcodex.com>
2019-03-04 21:12:19 +0100

    Simplify board reducer

--- src/reducers/BoardReducer.js
+++ src/reducers/BoardReducer.js
@@ -2,49 +2,45 @@ import InitialState from "./InitialState";
 import * as actions from "../constants/actionTypes";
 import * as utilActions from "../utils/array";
 
+function updateState(oldValues, newValues) {
+  return Object.assign([], oldValues, newValues);
+}
+
 export default function BoardReducer(state = InitialState.board, action) {
   switch (action.type) {
     case actions.RESET_BOARD: return utilActions.getShuffled(action.difficulty, action.x);
 
     case actions.FLIP_CARD: {
-      const newState = Object.assign([], state);
+      const newState = state[action.ylocation][action.xlocation].flipped = true;
 
-      newState[action.ylocation][action.xlocation].flipped = true;
-
-      return newState;
+      return updateState(state, newState);
     }
 
     case actions.FLIP_BACK: {
-      const newState = Object.assign([], state);
-
-      action.locations.forEach(location => {
-        return newState[location.y][location.x].flipped = false;
+      const newState = action.locations.forEach(location => {
+        state[location.y][location.x].flipped = false;
       });
 
-      return newState;
+      return updateState(state, newState);
     }
 
     case actions.SET_CARD_TO_RESOLVED: {
-      const newState = Object.assign([], state);
-
-      action.locations.forEach(location => {
-        return newState[location.y][location.x].resolved = true;
+      const newState = action.locations.forEach(location => {
+        state[location.y][location.x].resolved = true;
       });
 
-      return newState;
+      return updateState(state, newState);
     }
 
     case actions.HIDE_ALL: {
-      const newState = Object.assign([], state);
-
-      newState.map(row => {
+      const newState = state.map(row => {
         return row.map(col => {
           col.flipped = false;
           return col;
         });
       });
 
-      return newState;
+      return updateState(state, newState);  
     }
 
     default: return state;
